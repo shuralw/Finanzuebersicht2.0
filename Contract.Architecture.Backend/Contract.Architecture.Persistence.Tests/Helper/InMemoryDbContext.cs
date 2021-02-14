ac5ded2226e177b186b72b48e30fce53e6bd9ff3
Jonas Hammerschmidt <jonas.hammerschmidt@outlook.de>
2021-02-14 11:33:22 +0100

    Improved structure of Persistence.Tests

---
+++ Contract.Architecture.Backend/Contract.Architecture.Persistence.Tests/Helper/InMemoryDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Contract.Architecture.Persistence.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contract.Architecture.Persistence.Tests
+{
+    public static class InMemoryDbContext
+    {
+        public static PersistenceDbContext CreatePersistenceDbContext()
+        {
+            DbContextOptions<PersistenceDbContext> options;
+            var builder = new DbContextOptionsBuilder<PersistenceDbContext>();
+            builder.UseInMemoryDatabase("in-memory-db");
+            options = builder.Options;
+
+            PersistenceDbContext persistenceDbContext = PersistenceDbContext.CustomInstantiate(options);
+            persistenceDbContext.Database.EnsureDeleted();
+            persistenceDbContext.Database.EnsureCreated();
+
+            return persistenceDbContext;
+        }
+    }
+}
