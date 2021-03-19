f895bacf793f8161d7dd42c51a0b6764763197fe
Jonas Hammerschmidt <jonas.hammerschmidt@outlook.de>
2021-03-19 22:09:07 +0100

    Added SearchDropdownComponent

--- Contract.Architecture.Frontends/Contract.Architecture.Web.Core/src/app/components/ui/ui-components.module.ts
+++ Contract.Architecture.Frontends/Contract.Architecture.Web.Core/src/app/components/ui/ui-components.module.ts
@@ -1,18 +1,28 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
 import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
+import { SearchDropdownComponent } from './search-dropdown/search-dropdown.component';
 
 @NgModule({
     declarations: [
         LoadingSpinnerComponent,
+        SearchDropdownComponent,
     ],
     imports: [
         CommonModule,
-        FormsModule
+        FormsModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        NgxMatSelectSearchModule,
     ],
     exports: [
         LoadingSpinnerComponent,
+        SearchDropdownComponent,
     ]
 })
 export class UiComponentsModule { }
