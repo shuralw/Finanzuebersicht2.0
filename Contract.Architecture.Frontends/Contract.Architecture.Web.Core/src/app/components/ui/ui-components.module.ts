5f50a03f100d2b0ae298433d1f064377a684d0b4
Jonas Hammerschmidt <jonas.hammerschmidt@outlook.de>
2021-05-11 10:44:47 +0200

    Added Confirmation Dialog

--- Contract.Architecture.Frontends/Contract.Architecture.Web.Core/src/app/components/ui/ui-components.module.ts
+++ Contract.Architecture.Frontends/Contract.Architecture.Web.Core/src/app/components/ui/ui-components.module.ts
@@ -1,10 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
 import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
+import { ConfirmationDialog } from './confirmation-dialog/confirmation-dialog.component';
+import { ConfirmationDialogService } from './confirmation-dialog/confirmation-dialog.service';
 import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
 import { SearchDropdownFormComponent } from './search-dropdown-form/search-dropdown-form.component';
 import { SearchDropdownComponent } from './search-dropdown/search-dropdown.component';
@@ -14,6 +18,7 @@ import { TableFilterBarComponent } from './table-filter-bar/table-filter-bar.com
 
 @NgModule({
     declarations: [
+        ConfirmationDialog,
         LoadingSpinnerComponent,
         SearchDropdownComponent,
         SearchDropdownFormComponent,
@@ -32,9 +37,14 @@ import { TableFilterBarComponent } from './table-filter-bar/table-filter-bar.com
         FormsModule,
         ReactiveFormsModule,
         MatFormFieldModule,
+        MatButtonModule,
+        MatDialogModule,
         MatSelectModule,
         MatIconModule,
         NgxMatSelectSearchModule,
+    ],
+    providers: [
+        ConfirmationDialogService,
     ]
 })
 export class UiComponentsModule { }
